62ab618415ad3045fcf887b547da391eed8badf7
Anna Lazurkevic <anialaz@gmail.com>
2021-11-10 12:08:48 +0200

    [8] :component representation

--- ccomponents/apartment-Card-Component.js
+++ ccomponents/apartment-Card-Component.js
@@ -7,9 +7,16 @@ class ApartCardComponent{
 
 
     init = ()=>{
+        const {type,owner,roomCount,squares,address,price,imgSrc}= this.props
         this.htmlElement = document.createElement('article')
         this.htmlElement.className = `card p-3 shadow  col-sm-2 col-xs-1 col-lg-3 col-lx-4 `
-        this.htmlElement.innerHTML = `FFF`
-
+        this.htmlElement.innerHTML = `
+        <img src="${imgSrc}" class="card-img-top" style="height:400px;object-fit: cover;" />
+        <div class="card-body">
+          <h5 class="card-title text-center">${type}</h5>
+          
+          </div>
+        </div>
+      `;
     }
 }
--- ccomponents/apartment-Grid-Component.js
+++ ccomponents/apartment-Grid-Component.js
@@ -9,7 +9,7 @@ class ApartGridComponent {
     }
 
     saveData = (apartment) => {
-        this.state = {apartment, loading:false};
+        this.state = { apartment, loading: false };
         this.render();
     }
     showError = error => { console.log(error) };
@@ -20,28 +20,28 @@ class ApartGridComponent {
         this.state.loading = true;
         this.getApart();
         this.htmlElement = document.createElement('div');
-        this.htmlElement.className ='row  g-3 '
+        this.htmlElement.className = 'row  g-3 '
         this.render()
     }
 
-// wrapChild = (htmlElement) => {
-//     const wrapper = document.createElement('div')
-//     wrapper.className = 'col'
-//     wrapper.append(htmlElement)
-//     return wrapper
-// }
-   
-    render = ()=>{
-        const {loading} = this.state;
-        if (loading){
-            this.htmlElement.innerHTML = `<div class = "text-center"><img src="assets/loading.png"/></div>`
-        }else{
-            
-     this.htmlElement.innerHTML =''   
-    const cardComponents = this.state.apartment.map(x=>new ApartCardComponent(x))
-    const cardElement = cardComponents.map(x=>x.htmlElement)
-    this.htmlElement.append(...cardElement)
+    wrapChild = (htmlElement) => {
+        const wrapper = document.createElement('div')
+        wrapper.className = 'col'
+        wrapper.append(htmlElement)
+        return wrapper
+    }
 
-       }
+    render = () => {
+        const { loading } = this.state;
+        if (loading) {
+            this.htmlElement.innerHTML = `<div class = "text-center"><img src="assets/loading.png"/></div>`
+        } else {
+
+            this.htmlElement.innerHTML = ''
+            const cardComponents = this.state.apartment.map(x => new ApartCardComponent(x))
+            const cardElement = cardComponents.map(x => x.htmlElement)
+            this.htmlElement.append(...cardElement)
+            const wrappedElements = cardElements.map(this.wrapChild)
+        }
     }
 }
